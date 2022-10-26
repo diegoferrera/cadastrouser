b0f234df6fd2cbd456be2b99fc923a8f7cfd1a49
Diego Ferreira <diegocpc@icloud.com>
2022-10-26 13:32:45 -0300

    novo4

--- src/pages/cardapio/alimentos.tsx
+++ src/pages/cardapio/alimentos.tsx
@@ -13,6 +13,7 @@ export default function Alimentos () {
             <Sidebar/>
             
             <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
+            <h4 className="mt-4 text-lg bg-red-500 rounded text-gray-100 font-bold leading-tight text">Carregando dados importados dos arquivos .doc, enviaremos um alerta ao concluir</h4>
                <Alimentoli/>
             </div>
         </div>
--- src/pages/suplementos/index.tsx
+++ src/pages/suplementos/index.tsx
@@ -13,6 +13,7 @@ export default function Suplementos () {
             <Sidebar/>
             
             <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
+            <h4 className="mt-4 text-lg bg-red-500 rounded text-gray-100 font-bold leading-tight text">Alerta do servidor Vercel, o carregamento é feito uma uníca vez</h4>
                <Suplementosli/>
             </div>
         </div>
